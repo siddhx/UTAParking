ee666298514abc8db9bd1e6286a6c33bba85e84a
root <root@Siddharth-Dell.localdomain>
2019-02-05 11:39:09 -0600

    adding the controllers

---
+++ src/UTAParking/Model/registerForm.java
@@ -0,0 +1,99 @@
+package UTAParking.Model;
+
+public class registerForm {
+	private String username="";
+	private String password="";
+	private String last_name="";
+	private String first_name="";
+	private String role="";
+	private String UTA_Id="";
+	private String phone="";
+	private String email="";
+	private String street_address="";
+	private String city="";
+	private String state="";
+	private String zipcode="";
+	private String Parking_Permit_Type="";
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getUTA_Id() {
+		return UTA_Id;
+	}
+	public void setUTA_Id(String uTA_Id) {
+		UTA_Id = uTA_Id;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getStreet_address() {
+		return street_address;
+	}
+	public void setStreet_address(String street_address) {
+		this.street_address = street_address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getParking_Permit_Type() {
+		return Parking_Permit_Type;
+	}
+	public void setParking_Permit_Type(String parking_Permit_Type) {
+		Parking_Permit_Type = parking_Permit_Type;
+	}
+	
+	
+	
+}
